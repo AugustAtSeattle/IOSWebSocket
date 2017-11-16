62423cb56de3e44d90ef0ead8feb7140642b1281
August <august.lin.usa@gmail.com>
2017-11-16 13:04:35 -0800

    Add web socket

--- WebSocketExample/ViewController.swift
+++ WebSocketExample/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import Starscream
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,WebSocketDelegate {
+    
 
+    
+    let socket = WebSocket(url: URL(string: "ws://demos.kaazing.com/echo")!)//WebSocket(url: URL(string: "ws://localhost:1337/")!, protocols: ["chat"])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("start")
+        socket.delegate = self
+        socket.connect()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //  release websocket
+    deinit {
+        socket.disconnect(forceTimeout: 0)
+        socket.delegate = nil
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //  WebSocket Delegate
+    func websocketDidConnect(socket: WebSocketClient) {
+        print("websocket is connected")
+        socket.write(string: "Ok Kara")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        print("websocket is disconnected: \(error?.localizedDescription)")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        print("got some text: \(text)")
+        wakeWordTextView.text = text
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        print("got some data: \(data.count)")
+    }
+    
+    func websocketDidReceivePong(socket: WebSocketClient, data: Data?) {
+        print("Got pong! Maybe some data: \(data?.count)")
+    }
+    // WebSocket Delegate Code End
 }
-
