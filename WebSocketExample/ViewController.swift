6f1933d23a32da101928a0f5025b98d535a07396
August <august.lin.usa@gmail.com>
2017-11-16 11:22:14 -0800

    Initial Commit

---
+++ WebSocketExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebSocketExample
+//
+//  Created by August on 11/16/17.
+//  Copyright © 2017 August. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
